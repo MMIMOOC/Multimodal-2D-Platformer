91f6703a1fe94e244dfac2ca49d685c58ec8c4ed
gogokotsev00 <Kotsev75@agmail.com>
2022-06-24 13:14:12 +0200

    Shooting is possible by pressing key V

--- 2D_Platformer/Assets/Scripts/Player/PlayerAttack.cs
+++ 2D_Platformer/Assets/Scripts/Player/PlayerAttack.cs
@@ -28,7 +28,7 @@ public class PlayerAttack : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetMouseButton(0) && cooldownTimer > attackCooldown && playerMovement.canAttack())
+        if (Input.GetKeyDown(KeyCode.V) && cooldownTimer > attackCooldown && playerMovement.canAttack())
             Attack();
 
         cooldownTimer += Time.deltaTime;
