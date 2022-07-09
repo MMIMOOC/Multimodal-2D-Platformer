21964b80143f8c90fda640cd51056ef68e89565b
gogokotsev00 <Kotsev75@agmail.com>
2022-07-10 01:45:19 +0200

    Added a trigger for the final screen

--- 2D_Platformer/Assets/Scripts/LoadingManager.cs
+++ 2D_Platformer/Assets/Scripts/LoadingManager.cs
@@ -3,9 +3,17 @@ using UnityEngine.SceneManagement;
 
 public class LoadingManager : MonoBehaviour
 {
+    [SerializeField] private GameWonScript finalScreen;
+    [SerializeField] private Health playerHealth; 
+
     private void Update()
     {
+        //restarts the level
         if (Input.GetKeyDown(KeyCode.F))
-            SceneManager.LoadScene(1);
+            SceneManager.LoadScene("Level1");
+    }
+
+    public void GameWon() {
+        finalScreen.Setup(playerHealth.currentHealth);
     }
 }
